112d227778551fdfeab386568f77d5e326e9383d
Juan Vallejo <bagy45@gmail.com>
2025-07-29 14:48:10 -0400

    Refactor accordion toggle functionality to use a single event listener for improved code efficiency and DRY principles

--- index.js
+++ index.js
@@ -1,52 +1,21 @@
-const button1 = document.getElementById('accordion-toggle1')
-const button2 = document.getElementById('accordion-toggle2')
-const button3 = document.getElementById('accordion-toggle3')
-const button4 = document.getElementById('accordion-toggle4')
+const imagePlus = 'assets/images/icon-plus.svg';
+const imageMinus = 'assets/images/icon-minus.svg';
 
-const text1 = document.getElementById('toggle-text1')
-const text2 = document.getElementById('toggle-text2')
-const text3 = document.getElementById('toggle-text3')
-const text4 = document.getElementById('toggle-text4')
+const allToggleButtons = document.querySelectorAll('.accordion-toggle');
+const allToggleTexts = document.querySelectorAll('.toggle-text');
 
-const image1 = 'assets/images/icon-plus.svg'
-const image2 = 'assets/images/icon-minus.svg'
+allToggleButtons.forEach((button, index) => {
+    const textElement = allToggleTexts[index];
 
-button1.addEventListener('click', () => {
-    if (getComputedStyle(text1).display === 'none') {
-        text1.style.display = 'block'
-        button1.src = image2
-    } else {
-        text1.style.display = 'none'
-        button1.src = image1
-    }
-})
-
-button2.addEventListener('click', () => {
-    if (getComputedStyle(text2).display === 'none') {
-        text2.style.display = 'block'
-        button2.src = image2
-    } else {
-        text2.style.display = 'none'
-        button2.src = image1
-    }
-})
-
-button3.addEventListener('click', () => {
-    if (getComputedStyle(text3).display === 'none') {
-        text3.style.display = 'block'
-        button3.src = image2
-    } else {
-        text3.style.display = 'none'
-        button3.src = image1
-    }
-})
-
-button4.addEventListener('click', () => {
-    if (getComputedStyle(text4).display === 'none') {
-        text4.style.display = 'block'
-        button4.src = image2
-    } else {
-        text4.style.display = 'none'
-        button4.src = image1
-    }
-})
+    button.addEventListener('click', () => {
+        if (getComputedStyle(textElement).display === 'none') {
+            textElement.style.display = 'block';
+            button.src = imageMinus;
+            button.alt = 'Collapse content';
+        } else {
+            textElement.style.display = 'none';
+            button.src = imagePlus;
+            button.alt = 'Expand content';
+        }
+    });
+});
